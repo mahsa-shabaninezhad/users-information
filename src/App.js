97e6430ce88ab5e0fd8724c55fa2449d2e929f86
Mahsa Shabaninezhad <mahsa.shabaninezhad@gmail.com>
2021-06-11 23:23:43 +0430

    adding page component to the App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import './App.css';
+import HomePage from './Pages/HomePage';
+import InformationPage from './Pages/InformationPage';
+import 'react-toastify/dist/ReactToastify.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <Switch> 
+          <Route exact component={HomePage} path="/"/>     
+          <Route exact component={InformationPage} path="/:id"/>     
+        </Switch>
+      </Router>
     </div>
   );
 }
